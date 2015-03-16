50e110bc23f34607474ab964493fd32c1e9bef27
Alexander Larsson <alexl@redhat.com>
2015-03-16 14:12:09 +0100

    Add method handling scaffolding

--- xdp-document.c
+++ xdp-document.c
@@ -30,15 +30,15 @@ xdp_document_finalize (GObject *object)
   XdpDocument *doc = (XdpDocument *)object;
 
   g_free (doc->url);
-  
+
   G_OBJECT_CLASS (xdp_document_parent_class)->finalize (object);
 }
 
 static void
 xdp_document_get_property (GObject    *object,
-			   guint       prop_id,
-			   GValue     *value,
-			   GParamSpec *pspec)
+                           guint       prop_id,
+                           GValue     *value,
+                           GParamSpec *pspec)
 {
   XdpDocument *doc = (XdpDocument *)object;
 
@@ -59,9 +59,9 @@ xdp_document_get_property (GObject    *object,
 
 static void
 xdp_document_set_property (GObject      *object,
-			   guint         prop_id,
-			   const GValue *value,
-			   GParamSpec   *pspec)
+                           guint         prop_id,
+                           const GValue *value,
+                           GParamSpec   *pspec)
 {
   XdpDocument *doc = (XdpDocument *)object;
 
@@ -93,22 +93,22 @@ xdp_document_class_init (XdpDocumentClass *klass)
 
   resource_class = GOM_RESOURCE_CLASS(klass);
   gom_resource_class_set_table(resource_class, "documents");
-  
+
   gParamSpecs [PROP_ID] =
     g_param_spec_int64 ("id", _("Id"), _("Unique Id"),
-			G_MININT64,
-			G_MAXINT64,
-			0,
-			(G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+                        G_MININT64,
+                        G_MAXINT64,
+                        0,
+                        (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
   g_object_class_install_property (object_class, PROP_ID,
                                    gParamSpecs [PROP_ID]);
   gom_resource_class_set_primary_key(resource_class, "id");
 
   gParamSpecs[PROP_URL] = g_param_spec_string("url", "Url",
-                                         "Location of data.",
-					NULL, G_PARAM_READWRITE);
+                                              "Location of data.",
+                                              NULL, G_PARAM_READWRITE);
   g_object_class_install_property (object_class, PROP_URL,
-				   gParamSpecs[PROP_URL]);
+                                   gParamSpecs[PROP_URL]);
   gom_resource_class_set_notnull(resource_class, "url");
 
 }
@@ -120,18 +120,77 @@ xdp_document_init (XdpDocument *self)
 
 XdpDocument *
 xdp_document_new (GomRepository *repo,
-		  const char *url)
+                  const char *url)
 {
   return g_object_new (XDP_TYPE_DOCUMENT,
-		       "repository", repo,
-		       "url", url);
+                       "repository", repo,
+                       "url", url);
 }
+
+gint64
+xdp_document_get_id (XdpDocument *doc)
+{
+  return doc->id;
+}
+
+static void
+xdp_document_handle_read (XdpDocument *doc,
+                          GDBusMethodInvocation *invocation,
+                          GVariant *parameters)
+{
+  const char *window;
+
+  g_variant_get (parameters, "(s)", &window);
+}
+
+struct {
+  const char *name;
+  const char *args;
+  void (*callback) (XdpDocument *doc,
+                    GDBusMethodInvocation *invocation,
+                    GVariant *parameters);
+} doc_methods[] = {
+  { "Read", "(s)", xdp_document_handle_read}
+};
 
 void
 xdp_document_handle_call (XdpDocument *doc,
-			  GDBusMethodInvocation *invocation)
+                          GDBusMethodInvocation *invocation)
 {
-  g_print ("handle call %s on id %d\n", g_dbus_method_invocation_get_method_name (invocation), (int)doc->id);
+  const char *method_name = g_dbus_method_invocation_get_method_name (invocation);
+  const gchar *interface_name = g_dbus_method_invocation_get_interface_name (invocation);
+  GVariant *parameters = g_dbus_method_invocation_get_parameters (invocation);
+  int i;
+
+  if (strcmp (interface_name, "org.freedesktop.portal.Document") == 0)
+    {
+      for (i = 0; i < G_N_ELEMENTS (doc_methods); i++)
+        {
+          if (strcmp (method_name, doc_methods[i].name) == 0)
+            {
+              if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE (doc_methods[i].args)))
+                {
+                  g_dbus_method_invocation_return_error (invocation,
+                                                         G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS,
+                                                         "Invalid arguments for %s.%s, expecting %s", interface_name, method_name, doc_methods[i].args);
+                  break;
+                }
+              else
+                {
+                  (doc_methods[i].callback) (doc, invocation, parameters);
+                }
+              break;
+            }
+        }
+      if (i == G_N_ELEMENTS (doc_methods))
+        g_dbus_method_invocation_return_error (invocation,
+                                               G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD,
+                                               "Method %s is not implemented on interface %s", method_name, interface_name);
+    }
+  else
+    g_dbus_method_invocation_return_error (invocation,
+                                           G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_INTERFACE,
+                                           "Interface %s not implemented", interface_name);
 }
 
 static void
@@ -139,7 +198,7 @@ ensure_documents (void)
 {
   if (documents == NULL)
     documents = g_hash_table_new_full (g_int64_hash, g_int64_equal,
-				       NULL, g_object_unref);
+                                       NULL, g_object_unref);
 }
 
 XdpDocument *
--- xdp-document.h
+++ xdp-document.h
@@ -10,13 +10,15 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE(XdpDocument, xdp_document, XDP, DOCUMENT, GomResource);
 
 XdpDocument *xdp_document_new (GomRepository *repo,
-			       const char *url);
+                               const char *url);
 
 XdpDocument *xdp_document_lookup (gint64 id);
 void xdp_document_insert (XdpDocument *doc);
+gint64 xdp_document_get_id (XdpDocument *doc);
+
 
 void xdp_document_handle_call (XdpDocument *doc,
-			       GDBusMethodInvocation *invocation);
+                               GDBusMethodInvocation *invocation);
 
 G_END_DECLS
 

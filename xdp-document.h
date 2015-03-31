b47537db1548d02aa91617b1f57a42e0e7b1d3eb
Matthias Clasen <mclasen@redhat.com>
2015-03-31 07:55:58 -0400

    Forgotten file

--- xdp-document.h
+++ xdp-document.h
@@ -19,7 +19,13 @@ XdpPermissionFlags xdp_document_get_permissions (XdpDocument *doc,
                                                  const char *app_id);
 void xdp_document_grant_permissions (XdpDocument *doc,
                                      const char *app_id,
-                                     XdpPermissionFlags perms);
+                                     XdpPermissionFlags perms,
+                                     GCancellable       *cancellable,
+                                     GAsyncReadyCallback callback,
+                                     gpointer            user_data);
+gint64 xdp_document_grant_permissions_finish (XdpDocument  *doc,
+                                              GAsyncResult  *result,
+                                              GError       **error);
 
 void xdp_document_handle_call (XdpDocument *doc,
                                GDBusMethodInvocation *invocation,

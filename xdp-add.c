b574e4f7241087018c8ccff410b722988ce55cd0
Alexander Larsson <alexl@redhat.com>
2015-04-13 15:21:56 +0200

    xdp-tool add: Make app-id optional

--- xdp-add.c
+++ xdp-add.c
@@ -11,7 +11,7 @@ do_add (int argc, char *argv[])
   GDBusConnection *bus;
   g_autoptr(GFile) file = NULL;
   g_autofree char *uri = NULL;
-  char *appid;
+  char *appid = NULL;
   gint64 handle;
   g_autoptr(GError) error = NULL;
   GVariant *ret;
@@ -20,15 +20,16 @@ do_add (int argc, char *argv[])
 
   setlocale (LC_ALL, "");
 
-  if (argc != 2)
+  if (argc < 1)
     {
-      g_printerr ("Usage: xdp add FILE APPID");
+      g_printerr ("Usage: xdp add FILE [APPID]");
       return 1;
     }
 
   file = g_file_new_for_commandline_arg (argv[0]);
   uri = g_file_get_uri (file);
-  appid = argv[1];
+  if (argc > 1)
+    appid = argv[1];
 
   bus = g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, &error);
   if (bus == NULL)
@@ -60,28 +61,31 @@ do_add (int argc, char *argv[])
 
   g_variant_unref (ret);
 
-  path = g_strdup_printf ("/org/freedesktop/portal/document/%ld", handle);
-
-  ret = g_dbus_connection_call_sync (bus,
-                                     "org.freedesktop.portal.DocumentPortal",
-                                     path,
-                                     "org.freedesktop.portal.Document",
-                                     "GrantPermissions",
-                                     g_variant_new ("(s^as)", appid, permissions),
-                                     G_VARIANT_TYPE ("(x)"),
-                                     G_DBUS_CALL_FLAGS_NONE,
-                                     30000,
-                                     NULL,
-                                     &error);
-  if (ret == NULL)
+  if (appid != NULL)
     {
-      g_printerr ("%s\n", error->message);
-      return 1;
+      path = g_strdup_printf ("/org/freedesktop/portal/document/%ld", handle);
+
+      ret = g_dbus_connection_call_sync (bus,
+                                         "org.freedesktop.portal.DocumentPortal",
+                                         path,
+                                         "org.freedesktop.portal.Document",
+                                         "GrantPermissions",
+                                         g_variant_new ("(s^as)", appid, permissions),
+                                         G_VARIANT_TYPE ("(x)"),
+                                         G_DBUS_CALL_FLAGS_NONE,
+                                         30000,
+                                         NULL,
+                                         &error);
+      if (ret == NULL)
+        {
+          g_printerr ("%s\n", error->message);
+          return 1;
+        }
+
+      g_variant_get (ret, "(x)", &handle);
+
+      g_print ("permission handle: %ld\n", handle);
     }
-
-  g_variant_get (ret, "(x)", &handle);
-
-  g_print ("permission handle: %ld\n", handle);
 
   return 0;
 }

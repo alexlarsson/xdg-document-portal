628ca4456e078b1eff769c3d48f12bacd48a0ec8
Alexander Larsson <alexl@redhat.com>
2015-06-22 13:36:00 +0200

    Reindent

--- xdp-document.h
+++ xdp-document.h
@@ -10,63 +10,57 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE(XdpDocument, xdp_document, XDP, DOCUMENT, GomResource);
 
-XdpDocument *xdp_document_new (GomRepository *repo,
-                               const char *uri);
-XdpDocument *xdp_document_new_with_title (GomRepository *repo,
-                                          const char *base_uri,
-                                          const char *title);
-
-gint64 xdp_document_get_id     (XdpDocument *doc);
-char * xdp_document_get_handle (XdpDocument *doc);
-
-XdpPermissionFlags xdp_document_get_permissions (XdpDocument *doc,
-                                                 const char *app_id);
-void xdp_document_grant_permissions (XdpDocument *doc,
-                                     const char *app_id,
-                                     XdpPermissionFlags perms,
-                                     GCancellable       *cancellable,
-                                     GAsyncReadyCallback callback,
-                                     gpointer            user_data);
-char *xdp_document_grant_permissions_finish (XdpDocument  *doc,
-					     GAsyncResult  *result,
-					     GError       **error);
-void xdp_document_revoke_permissions (XdpDocument *doc,
-                                      char *handle,
-                                      GCancellable *cancellable,
-                                      GAsyncReadyCallback callback,
-                                      gpointer user_data);
-gboolean xdp_document_revoke_permissions_finish (XdpDocument *doc,
-                                                 GAsyncResult *result,
-                                                 GError **error);
-
-void xdp_document_handle_call (XdpDocument *doc,
-                               GDBusMethodInvocation *invocation,
-                               const char *app_id);
-
-void xdp_document_load (GomRepository      *repository,
-                        const char         *handle,
-                        GCancellable       *cancellable,
-                        GAsyncReadyCallback callback,
-                        gpointer            user_data);
-XdpDocument * xdg_document_load_finish (GomRepository *repository,
-                                        GAsyncResult    *result,
-                                        GError         **error);
-void xdp_document_for_uri (GomRepository      *repository,
-                           const char         *uri,
-                           GCancellable       *cancellable,
-                           GAsyncReadyCallback callback,
-                           gpointer            user_data);
-XdpDocument * xdp_document_for_uri_finish (GomRepository *repository,
-                                           GAsyncResult    *result,
-                                           GError         **error);
-
-void
-xdp_document_for_uri_and_title (GomRepository      *repository,
-                                const char         *uri,
-                                const char         *title,
-                                GCancellable       *cancellable,
-                                GAsyncReadyCallback callback,
-                                gpointer            user_data);
+XdpDocument *      xdp_document_new                       (GomRepository          *repo,
+                                                           const char             *uri);
+XdpDocument *      xdp_document_new_with_title            (GomRepository          *repo,
+                                                           const char             *base_uri,
+                                                           const char             *title);
+gint64             xdp_document_get_id                    (XdpDocument            *doc);
+char *             xdp_document_get_handle                (XdpDocument            *doc);
+XdpPermissionFlags xdp_document_get_permissions           (XdpDocument            *doc,
+                                                           const char             *app_id);
+void               xdp_document_grant_permissions         (XdpDocument            *doc,
+                                                           const char             *app_id,
+                                                           XdpPermissionFlags      perms,
+                                                           GCancellable           *cancellable,
+                                                           GAsyncReadyCallback     callback,
+                                                           gpointer                user_data);
+char *             xdp_document_grant_permissions_finish  (XdpDocument            *doc,
+                                                           GAsyncResult           *result,
+                                                           GError                **error);
+void               xdp_document_revoke_permissions        (XdpDocument            *doc,
+                                                           char                   *handle,
+                                                           GCancellable           *cancellable,
+                                                           GAsyncReadyCallback     callback,
+                                                           gpointer                user_data);
+gboolean           xdp_document_revoke_permissions_finish (XdpDocument            *doc,
+                                                           GAsyncResult           *result,
+                                                           GError                **error);
+void               xdp_document_handle_call               (XdpDocument            *doc,
+                                                           GDBusMethodInvocation  *invocation,
+                                                           const char             *app_id);
+void               xdp_document_load                      (GomRepository          *repository,
+                                                           const char             *handle,
+                                                           GCancellable           *cancellable,
+                                                           GAsyncReadyCallback     callback,
+                                                           gpointer                user_data);
+XdpDocument *      xdg_document_load_finish               (GomRepository          *repository,
+                                                           GAsyncResult           *result,
+                                                           GError                **error);
+void               xdp_document_for_uri                   (GomRepository          *repository,
+                                                           const char             *uri,
+                                                           GCancellable           *cancellable,
+                                                           GAsyncReadyCallback     callback,
+                                                           gpointer                user_data);
+XdpDocument *      xdp_document_for_uri_finish            (GomRepository          *repository,
+                                                           GAsyncResult           *result,
+                                                           GError                **error);
+void               xdp_document_for_uri_and_title         (GomRepository          *repository,
+                                                           const char             *uri,
+                                                           const char             *title,
+                                                           GCancellable           *cancellable,
+                                                           GAsyncReadyCallback     callback,
+                                                           gpointer                user_data);
 
 G_END_DECLS
 
